c9709ed8e07f2368f3f47b65cb041ba21dbb087f
Murtaza <Murtaza_taloo@hotmail.com>
2019-11-04 11:12:30 +1100

    stored user rating in user's rating column

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,24 +1,28 @@
 class ProfilesController < ApplicationController
   
   def user_profile
-    @user_rating = current_user.rating_scores_array.sum / current_user.rating_scores_array.length.to_f
-    
+  
+    # finding all the current listings of the current user
     @current_listings = current_user.listings.map do |l|
       if l.sold_status == nil
         l
       end
     end
 
+    # finding all the purchased listings of the current user
     @purchases = current_user.transactions.map do |transaction|
       transaction.listing
+      byebug
     end
 
+    # finding all the sold listings of the current user
     @sold_listings = current_user.listings.map do |l|
       if l.sold_status == true
         l
       end
     end
 
+    # calculating total earnings by summing the prices of all the sold listigns of current user
     @sales = @sold_listings.map do |listing|
       if listing.price != nil
         return listing.price
@@ -34,9 +38,14 @@ class ProfilesController < ApplicationController
     @user = User.find(params[:user_id])
     @listing = Listing.find(params[:listing_id])
 
+    # using an array (rating_scores_array) to store all individual rating scores and then calculating the rating and storing it in the user rating column 
+
     @user.rating_scores_array << params[:rating_scores_array]
     @user.save
+    @user.rating = @user.rating_scores_array.sum / @user.rating_scores_array.length.to_f
+    @user.save
 
+    # changing the rated_status in given listing so it can only be rated once
     @listing.rated_status = true
     @listing.save
 
